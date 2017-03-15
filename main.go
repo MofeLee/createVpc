147fccc82b373a0beac0f45351fe44ea7c3cd3b8
Mofe Lee <mofe@me.com>
2017-03-15 23:52:12 +0800

    format main

--- main.go
+++ main.go
@@ -14,6 +14,10 @@ var ACCESS_KEY_SECRET = os.Getenv("ACCESS_KEY_SECRET")
 const region = common.Region("cn-shanghai")
 
 func main() {
+	if ACCESS_KEY_ID == "" || ACCESS_KEY_SECRET == "" {
+		return
+	}
+
 	client := ecs.NewClient(ACCESS_KEY_ID, ACCESS_KEY_SECRET)
 
 	// 获取vpcId
@@ -27,22 +31,38 @@ func main() {
 	spew.Dump(vpcId)
 	spew.Dump(zoneId)
 
-	vswidthId, _ := client.CreateVSwitch(&ecs.CreateVSwitchArgs{ZoneId: zoneId, VpcId: vpcId, CidrBlock: "10.99.0.0/24"})
+	vswidthId, _ := client.CreateVSwitch(&ecs.CreateVSwitchArgs{
+		ZoneId:    zoneId,
+		VpcId:     vpcId,
+		CidrBlock: "10.99.0.0/24",
+	})
 
 	spew.Dump(vswidthId)
 	if vswidthId == "" {
 		return
 	}
 
-	vswitches, _, _ := client.DescribeVSwitches(&ecs.DescribeVSwitchesArgs{VpcId: vpcId, VSwitchId: vswidthId})
+	vswitches, _, _ := client.DescribeVSwitches(&ecs.DescribeVSwitchesArgs{
+		VpcId:     vpcId,
+		VSwitchId: vswidthId,
+	})
 	for vswitches[0].Status == "Pending" {
 		spew.Dump("Sleep 5 second")
 		time.Sleep(5 * time.Second)
-		vsws, _, _ := client.DescribeVSwitches(&ecs.DescribeVSwitchesArgs{VpcId: vpcId, VSwitchId: vswidthId})
+		vsws, _, _ := client.DescribeVSwitches(&ecs.DescribeVSwitchesArgs{
+			VpcId:     vpcId,
+			VSwitchId: vswidthId,
+		})
 		vswitches = vsws
 	}
 
-	instanceId, e := client.CreateInstance(&ecs.CreateInstanceArgs{RegionId: region, ImageId: "centos_7_2_64_40G_base_20170222.vhd", InstanceType: "ecs.n1.tiny", HostName: "test-ecs", VSwitchId: vswidthId})
+	instanceId, e := client.CreateInstance(&ecs.CreateInstanceArgs{
+		RegionId:     region,
+		ImageId:      "centos_7_2_64_40G_base_20170222.vhd",
+		InstanceType: "ecs.n1.tiny",
+		HostName:     "test-ecs",
+		VSwitchId:    vswidthId,
+	})
 
 	spew.Dump("Done", instanceId, e)
 }
